800f11d92b4dbed881b5306f89affa6e5dbdacb7
Ititi Obidah <iobidah9@gmail.com>
2019-04-27 00:33:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,27 +1,52 @@
-def roll_call_dwarves(dwarves)
-  dwarves.each_with_index do |name, i|
-   puts  "#{i + 1}. #{name}"
+def roll_call_dwarves(dwarf)
+  dwarf[0..dwarf.size/2].each_with_index do |name, i|
+    puts "#{i + 1} #{name}"
+end 
  end
-end
-
-def summon_captain_planet(planeteer_calls)
-  planeteer_calls.collect do |calls|
-    calls.capitalize + "!"
+ 
+def summon_captain_planet(calls)
+  calls.collect do |word|
+    word.capitalize + "!"
   end
 end
 
 def long_planeteer_calls(calls)
-  if calls.any? do |word|
-    word.length > 4 
+  short_calls = false 
+  calls.each do |word|
+    if word.length > 4
+      short_calls = true 
   end
-    return true
-  else false 
-  end
-end
+ end
+ short_calls 
+ end
 
 def find_the_cheese(cheese)
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types = ["cheddar", "gouda", "camebert", "swiss"]
   cheese.find do |cheese|
-  cheese_types.include?(cheese)
+    cheese_types.include?(cheese)
+  end
+end 
+
+
+
+
+
+
+
+def words_that_start_with_b(words)
+  words.select do |word|
+    word.start_with?("b")
+ end
 end
-end
+
+words_that_start_with_b(['banana', 'cherry', 'gouda']) # ['banana']
+words_that_start_with_b(['brian', 'james', 'billy']) # ['brian', 'billy']
+words_that_start_with_b(['james', 'arren', 'charles']) # []
+
+
+
+
+
+
+
+
